9cee48328f063ad333c0b408e45f28e117a7aed5
chungcf2006 <chungcf2006@gmail.com>
2016-08-08 14:01:41 +0800

    Added Bot Token

--- common/telegram.lib.php
+++ common/telegram.lib.php
@@ -1,5 +1,5 @@
 <?php
-	define('BOT_TOKEN', 'TOKEN_HERE');
+	define('BOT_TOKEN', '265746448:AAGBWXTbnTtu0er6YVWHH8DpOnFXGVqBCRk');
 	define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 	//define('PROXY', 'proxy.cse.cuhk.edu.hk:8000');
 
